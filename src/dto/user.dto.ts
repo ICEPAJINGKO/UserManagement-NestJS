9cb29807434db72d5288231c9893d0cdcdc55bc2
ICEPAJINGKO <icepajingko@gmail.com>
2022-07-27 14:23:09 +0700

    use dto

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -10,9 +10,28 @@ export class userDTO {
 
   @IsString()
   @MaxLength(3)
-  age: string;
+  age: number;
 
   @IsString()
   @MaxLength(3)
   position: string;
 }
+
+export class createUserDTO {
+    @IsString()
+    @MaxLength(3)
+    id: string;
+  
+    @IsString()
+    @MaxLength(3)
+    name: string;
+  
+    @IsString()
+    @MaxLength(3)
+    age: number;
+  
+    @IsString()
+    @MaxLength(3)
+    position: string;
+  }
+  
