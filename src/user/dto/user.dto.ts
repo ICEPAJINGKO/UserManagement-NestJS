947d4c530d22ecd418727262891eb17b7ea1a3a6
ICEPAJINGKO <icepajingko@gmail.com>
2023-06-14 10:30:00 +0700

    fixed format

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,37 +1,36 @@
 import { IsString, MaxLength } from 'class-validator';
 export class userDTO {
-  @IsString()
-  @MaxLength(3)
-  id: string;
+    @IsString()
+    @MaxLength(3)
+    id: string;
 
-  @IsString()
-  @MaxLength(3)
-  name: string;
+    @IsString()
+    @MaxLength(3)
+    name: string;
 
-  @IsString()
-  @MaxLength(3)
-  age: number;
+    @IsString()
+    @MaxLength(3)
+    age: number;
 
-  @IsString()
-  @MaxLength(3)
-  position: string;
+    @IsString()
+    @MaxLength(3)
+    position: string;
 }
 
 export class createUserDTO {
     @IsString()
     @MaxLength(3)
     id: string;
-  
+
     @IsString()
     @MaxLength(3)
     name: string;
-  
+
     @IsString()
     @MaxLength(3)
     age: number;
-  
+
     @IsString()
     @MaxLength(3)
     position: string;
-  }
-  
+}
