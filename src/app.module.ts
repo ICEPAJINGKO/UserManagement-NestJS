b056dc956fef44838f2a1cb75a3480c4fe8460a3
ICEPAJINGKO <icepajingko@gmail.com>
2023-06-16 01:24:33 +0700

    added config module from @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 import { MongodbModule } from './mongodb/mongodb.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/usermanagement_nestjs'),
-    UserModule,
-    MongodbModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV || 'development'}` }),
+        MongooseModule.forRoot('mongodb://localhost:27017/usermanagement_nestjs'),
+        UserModule,
+        MongodbModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  console.log('\x1b[36m', `The service is currently running in the ${configService.get('NODE_ENV')} environment.`, '\x1b[0m');
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
